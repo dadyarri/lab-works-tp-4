d34dadfc0fbb08857857866c113be244a611b3bb
Daniil <dadyarri@gmail.com>
2022-05-17 20:44:42 +0300

    lw6: update database schema

--- lw6/Data/Migrations/20220517172740_update.Designer.cs
+++ lw6/Data/Migrations/20220517172740_update.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using lw6.Models;
@@ -11,9 +12,10 @@ using lw6.Models;
 namespace lw6.Data.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220517172740_update")]
+    partial class update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
