6247d6acce14fb485c2088b1bc44817ab97211d6
Daniil <dadyarri@gmail.com>
2022-05-18 20:29:04 +0300

    lw7: add developer table; migrate database

--- lw7/Data/Migrations/20220518172716_add-developer.Designer.cs
+++ lw7/Data/Migrations/20220518172716_add-developer.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using lw7.Models;
@@ -11,9 +12,10 @@ using lw7.Models;
 namespace lw7.Data.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220518172716_add-developer")]
+    partial class adddeveloper
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
