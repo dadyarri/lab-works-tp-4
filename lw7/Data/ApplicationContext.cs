56ed82f1b9a9bc64d8299d7de9f9fe2522edc06f
Daniil <dadyarri@gmail.com>
2022-05-25 16:19:11 +0300

    lw7: rename context

--- lw7/Data/ApplicationContext.cs
+++ lw7/Data/ApplicationContext.cs
@@ -1,24 +1,17 @@
+using lw7.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace lw7.Models;
+namespace lw7.Data
+;
 
-public sealed class GameContext : IdentityDbContext
+public class ApplicationContext: IdentityDbContext
 {
-    public DbSet<Game>? Games { get; set; }
-    public DbSet<Developer>? Developers { get; set; }
+    public DbSet<Game> Games { get; set; }
 
-    public GameContext(DbContextOptions<GameContext> options) : base(options)
+    public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
     {
         Database.EnsureCreated();
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Game>()
-            .HasOne(g => g.Developer)
-            .WithMany(d => d.Games);
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
